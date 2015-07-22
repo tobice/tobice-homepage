88fe5704c81102cb93d0d77e13824099c2efc8c4
Tobiáš Potoček <tobiaspotocek@gmail.com>
2015-07-22 20:35:40 +0200

    Added source maps to the build

--- webpack/build.js
+++ webpack/build.js
@@ -4,6 +4,7 @@ var loaders = require('./loaders.js');
 
 module.exports = {
     entry: __dirname + '/../index.js',
+    devtool: 'source-map',
     module: {
         loaders: loaders
     },
@@ -13,6 +14,9 @@ module.exports = {
     },
     plugins: [
         new ExtractTextPlugin("bundle.css"),
-        // new optimize.UglifyJsPlugin()
+        new optimize.UglifyJsPlugin({
+            sourceMap: true,
+            mangle: false // it breaks the Immutable Record
+        })
     ]
 };
