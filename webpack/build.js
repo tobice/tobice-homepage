7126a76951fe79b4e05a1e71a6568f8ef287e95a
Tobiáš Potoček <tobiaspotocek@gmail.com>
2015-07-25 19:05:08 +0200

    Removed source maps from build; fixed company name typo

--- webpack/build.js
+++ webpack/build.js
@@ -4,7 +4,6 @@ var loaders = require('./loaders.js');
 
 module.exports = {
     entry: __dirname + '/../index.js',
-    devtool: 'source-map',
     module: {
         loaders: loaders
     },
@@ -15,7 +14,6 @@ module.exports = {
     plugins: [
         new ExtractTextPlugin("bundle.css"),
         new optimize.UglifyJsPlugin({
-            sourceMap: true,
             mangle: false // it breaks the Immutable Record
         })
     ]
